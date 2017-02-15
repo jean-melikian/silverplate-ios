65736690921dd91f28901c5e56b7b958be2ee5a4
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-02-15 16:21:45 +0100

    Fix scripts

--- travis/fastlane_deploy.sh
+++ travis/fastlane_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Currently, you are on the ${TRAVIS_BRANCH} branch."
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
   fastlane deploy
 fi
--- travis/fastlane_test.sh
+++ travis/fastlane_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Currently, you are on the ${TRAVIS_BRANCH} branch."
 if [[ "${TRAVIS_BRANCH}" == "master" || "${TRAVIS_BRANCH}" == "develop" ]]; then
   fastlane test
 fi
