46682e1b2235dd4f981aa23d16b8c09b37347189
Jean-Christophe MELIKIAN <jean.melikian@gmail.com>
2017-02-15 02:51:40 +0100

    Connectivity class + Optimized and easier public API

--- SilverPlate/SilverPlate.h
+++ SilverPlate/SilverPlate.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double SilverPlateVersionNumber;
 FOUNDATION_EXPORT const unsigned char SilverPlateVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SilverPlate/PublicHeader.h>
-
-
